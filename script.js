602be9d684a053e0b8c5421dff4a83124c0a96bc
Parker Barnett <parkerbarnett02@gmail.com>
2023-03-17 11:26:35 -0400

    Finished Exercise

---
+++ script.js
@@ -0,0 +1,50 @@
+// 1a
+let warmHugs = "Hi, I\'m Olaf and I like warm hugs.";
+
+// 1b
+console.log(warmHubs.toUpperCase());
+
+// 1c
+warmHugs = warmHugs.replace("like", "love")
+console.log(warmHugs);
+
+// 2a
+let beenImpaled = "Oh, look at that. I\'ve been impaled.";
+
+// 2b
+console.log(beenImpaled.slice(18, 36));
+
+// 3a
+let dotDotDot = "...";
+
+// 3b 
+let skullBones = '"I don't have a skull ${dotDotDot} or bones;
+console.log(skull);
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.floor(Math.random() * 3) + 1;
+console.log(randomNumber);
+
+// Bonus
+// 6
+console.log("Let It Go!".repeat(2).toUpperCase().trim());
+
+// 7a
+let reindeers = "Reindeers are better than people."
+
+// 7b
+console.log(reindeers.replace.All(" ", "_")); 
+
+// 8
+console.log(Math.SQRT2);
+
+// 9
+let newRandomNumber = Math.floor(Math.random() * 17); + 7
+console.log(newRandomNumber);
+
+
+
+
